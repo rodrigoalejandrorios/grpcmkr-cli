878d041053e2667350118e6b80bb4bcc950640f4
Rodrigo Rios <rodrigo.rios@naranja.com>
2023-04-18 07:54:25 -0300

    Finish library

--- bin/grpcmkr.ts
+++ bin/grpcmkr.ts
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 import { program } from "commander";
 import { CommandLoader } from "../commands/loader.command";
+import chalk from 'chalk';
 
 const bootstrap = async () => {
-  console.log('Desde webpack2')
+  const colorCodrr = chalk.hex('#C8FA5F')
+  console.log(colorCodrr('GrpcMKR!'))
   program
     .version(
-      require("../package.json").version,
+      `Version ${require("../../package.json").version}`,
       "-v, --version",
       "Output the current version."
     )
